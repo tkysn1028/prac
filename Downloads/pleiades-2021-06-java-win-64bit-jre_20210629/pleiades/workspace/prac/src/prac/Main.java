01a70bdab726ce47a5cca735ccdde26c9b80327b
tkysn1028 <tkysn1028@gmail.com>
2021-10-07 11:21:00 +0900

    cyclicbarrierまで勉強したよ

--- Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/Main.java
+++ Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/Main.java
@@ -1,15 +1,19 @@
 package prac;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Thread t = new Thread(() -> {
-			System.out.println("sada");
-		});
-		
-		t.start();
-		System.out.println("main");
-		
-		
+		ExecutorService exec = Executors.newFixedThreadPool(5);
+		CyclicBarrier barrier = new  CyclicBarrier(5, () -> {
+			System.out.println("all done!");
+		});		
+		for(int i = 0; i< 5 ; i++) {
+			exec.submit(new SampleThread(barrier));
+			
+		}
 }
 }
--- Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/SampleThread.java
+++ Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/SampleThread.java
@@ -1,37 +1,37 @@
 package prac;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
-public class SampleThread{
-	public static void main(String[] args) {
-
-		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-		exec.schedule(() -> {
-			System.out.println("finish");
-			exec.shutdown();
-		}, 10, TimeUnit.SECONDS);
-		
-		int count = 0;
-		while(true) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-			}
-	
-		if(exec.isShutdown()) {
-			break;
+public class SampleThread implements Runnable{
+		private CyclicBarrier barrier;
+		public SampleThread(CyclicBarrier barrier) {
+		super();
+		this.barrier = barrier;
 		}
-		System.out.println((++count) * 100 + " ms");
+	@Override
+	public void run() {
+		long id =Thread.currentThread().getId();
+		System.out.println("start : " + id);
+		int r = new Random().nextInt(10);
+		try {
+			System.out.println(r);
+			Thread.sleep(r * 1000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		System.out.println("end : "  + id);
 		
+		try {
+			this.barrier.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		
+		
+		
+		
+		}
 	}
-	
-	}
-	
 }
-
-	
