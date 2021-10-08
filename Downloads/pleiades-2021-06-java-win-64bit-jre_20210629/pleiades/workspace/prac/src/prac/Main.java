4f84577b248ffcf3776bf68bbd2a0716e2777253
tkysn1028 <tkysn1028@gmail.com>
2021-10-08 19:18:03 +0900

    consumer functional interface

--- Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/Main.java
+++ Downloads/pleiades-2021-06-java-win-64bit-jre_20210629/pleiades/workspace/prac/src/prac/Main.java
@@ -1,19 +1,8 @@
 package prac;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
-
 	public static void main(String[] args) {
-		ExecutorService exec = Executors.newFixedThreadPool(5);
-		CyclicBarrier barrier = new  CyclicBarrier(5, () -> {
-			System.out.println("all done!");
-		});		
-		for(int i = 0; i< 5 ; i++) {
-			exec.submit(new SampleThread(barrier));
-			
-		}
-}
+	}
+	
+	
 }
